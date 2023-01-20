0d2670afb973322aa084e824c5f0424d65376746
mehmet basat <mehmetbasatt@gmail.com>
2023-01-20 18:02:05 +0300

    JS Day 1

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-console.log("Merhaba Kodlama.io")
+console.log("what up, bitch!!!")
 
-//JS type safe değildir
+// JS type safe değildir
+
 let dolarBugun = 9.30
+var dolarDun = 9.20
 
-let dolarDun = 9.20
-dolarDun = "9.20"
 {
-    let dolarDun = 9.10
+    var dolarDun = 9.10
 }
 
 console.log(dolarDun)
@@ -18,19 +18,12 @@ console.log(euroDun)
 
 //array
 //camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
 
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+
+for(let i = 0; i<konutKredileri.length; i++) {
+    console.log("<li>" +konutKredileri[i]+ "</li>")
 }
+
 console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
